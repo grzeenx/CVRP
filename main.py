5ef21bcebf2aaa27a10053791b7dff422a5c7759
Grzegorz_Kacprowicz <kacprowiczg@student.mini.pw.edu.pl>
2020-03-12 20:59:56 +0100

    Add rank AS

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import os
 from Graph import Graph
 from greedy import GreedyAlgorithm
 from standars_as import StandardAntAlgorithm
-from elitist import  ElitistAntAlgorithm
-
+from elitist import ElitistAntAlgorithm
+from rank import RankAntAlgorithm
 
 dataset_path = os.path.join(pathlib.Path().absolute(), "dataset")
 g = Graph.load_graph("E-n22-k4.txt", dataset_path)
@@ -17,7 +17,10 @@ standard_as1 = StandardAntAlgorithm(g, 50, max_iterations=300, rho=0)
 result = standard_as1.execute()
 print(result)
 
-
 standard_as1 = ElitistAntAlgorithm(g, 50, max_iterations=300, rho=0)
+result = standard_as1.execute()
+print(result)
+
+standard_as1 = RankAntAlgorithm(g, 50, chosen_ants_count=15, max_iterations=300, rho=0)
 result = standard_as1.execute()
 print(result)
---
+++ rank.py
@@ -0,0 +1,32 @@
+from standars_as import StandardAntAlgorithm
+import math
+
+
+class RankAntAlgorithm(StandardAntAlgorithm):
+    def __init__(self, graph, ants_count, chosen_ants_count, max_iterations=50, alpha=1, beta=1, rho=0):
+
+        super().__init__(graph, ants_count, max_iterations, alpha, beta, rho)
+        self.chosen_ants_count = chosen_ants_count
+        self.best_costs_and_routes = [(math.inf, [[]])]
+
+    def perform_iteration_for_all_ants(self):
+        for ant in self.ants:
+            route, cost = ant.perform_iteration()
+            if cost < self.best_costs_and_routes[-1][0]:
+                self.best_costs_and_routes[-1] = (cost, route)
+                self.best_costs_and_routes.sort(key=lambda tup: tup[0])
+        self.evaporate_pheromones()
+        self.place_best_pheromones()
+        if self.best_costs_and_routes[0][0] < self.best_cost:
+            self.best_cost = self.best_costs_and_routes[0][0]
+            self.best_route = self.best_costs_and_routes[0][1]
+
+    def place_best_pheromones(self):
+        for i, cost_and_route in enumerate(self.best_costs_and_routes):
+            value_to_add = (self.chosen_ants_count - i) / cost_and_route[0]
+            for cycle in cost_and_route[1]:
+                for vertex_from, vertex_to in zip(cycle, cycle[1:]):
+                    self.pheromones[vertex_from, vertex_to] += value_to_add
+                    self.pheromones[vertex_to, vertex_from] += value_to_add
+                self.pheromones[cycle[-1], cycle[0]] += value_to_add
+                self.pheromones[cycle[0], cycle[-1]] += value_to_add
