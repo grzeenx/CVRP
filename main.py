897f595efb094f91aa262480d2c6e10febf57cf9
Grzegorz_Kacprowicz <kacprowiczg@student.mini.pw.edu.pl>
2020-03-31 21:30:04 +0200

    Make main nicer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from test_class import Test
 
-test1 = Test("E-n22-k4.txt", max_iterations=50, ant_count=75, chosen_ants_count=10, alpha=0.55, beta=0.85, gamma=0.5,
-             rho=0, exp_seed=100, alpha_ant=1, beta_ant=1)
-# test2 = Test("E-n22-k4.txt", max_iterations=100, ant_count=150, chosen_ants_count=10, alpha=0.55, beta=0.85, gamma=0.5,
-#              rho=0, exp_seed=100, alpha_ant=1, beta_ant=1)
-
-test1.run()
+Test("E-n22-k4.txt", max_iterations=50, ant_count=75, chosen_ants_count=10, alpha=0.55, beta=0.85, gamma=0.5,
+     rho=0, exp_seed=100, alpha_ant=1, beta_ant=1).run()
+# Test("E-n22-k4.txt", max_iterations=100, ant_count=150, chosen_ants_count=10, alpha=0.55, beta=0.85, gamma=0.5,
+#      rho=0, exp_seed=100, alpha_ant=1, beta_ant=1).run()
