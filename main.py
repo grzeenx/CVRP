6894b8861cd0c2814fbd4f8bdd48049151040b35
Grzegorz_Kacprowicz <kacprowiczg@student.mini.pw.edu.pl>
2020-04-06 02:17:51 +0200

    Use cross products instead of 7 loops

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from itertools import product
 from test_class import Test
 import csv
 
@@ -16,7 +17,7 @@ algorithms = ["standard", "elitist"]
 files = ["E-n22-k4.txt", "E-n33-k4.txt", "E-n51-k5.txt", "E-n76-k8.txt", "E-n76-k10.txt", "E-n101-k8.txt",
          "E-n101-k14.txt"]
 ant_counts = [50, 100, 200]
-max_iterations = [30, 100, 150]
+max_iterations_list = [30, 100, 150]
 seeds = [6666, 7777, 8888]
 rhos = [0, 0.3, 0.7]
 alphas_ant = [0.4, 0.8, 1]
@@ -28,23 +29,18 @@ betas_pso = [0.4, 0.8, 1]
 gammas_pso = [0.3, 0.7]
 
 counter = 0
-for i, file in enumerate(files):
+for file in files:
     result_filename = f"res_{file}_{'_'.join(algorithms)}.txt"
     create_file(result_filename)
-    for exp_seed in seeds:
-        print("seed ", exp_seed)
-        for max_iterations in max_iterations:
-            for ant_count in ant_counts:
-                print("ant_count", ant_count)
-                for alpha_ant in alphas_ant:
-                    for beta_ant in betas_ant:
-                        for rho in rhos:
-                            Test("E-n22-k4.txt", max_iterations=max_iterations, ant_count=ant_count,
-                                 chosen_ants_count=10, alpha=0.55, beta=0 / 85,
-                                 gamma=0.5, rho=rho, exp_seed=exp_seed, alpha_ant=alpha_ant, beta_ant=beta_ant).run(
-                                path=result_filename, algorithms=algorithms)
-                            counter += 1
-                            print(f"Iteration: {counter}")
+    for exp_seed, max_iterations, ant_count, alpha_ant, beta_ant, rho in product(seeds, max_iterations_list,
+                                                                                 ant_counts, alphas_ant, betas_ant,
+                                                                                 rhos):
+        Test("E-n22-k4.txt", max_iterations=max_iterations, ant_count=ant_count,
+             chosen_ants_count=None, alpha=None, beta=None,
+             gamma=None, rho=rho, exp_seed=exp_seed, alpha_ant=alpha_ant, beta_ant=beta_ant).run(
+            path=result_filename, algorithms=algorithms)
+        counter += 1
+        print(f"{counter}) {file}_s{exp_seed}_its{max_iterations}_ants{ant_count}_a{alpha_ant}_b{beta_ant}_r{rho}")
 
 # alpha 0.55
 # beta =0.85
