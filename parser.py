b14ff1f2c792a38876d76b07f5006e4acc5f7d36
Grzegorz_Kacprowicz <kacprowiczg@student.mini.pw.edu.pl>
2020-04-15 04:46:09 +0200

    Calculate average

--- parser.py
+++ parser.py
@@ -40,7 +40,7 @@ n101s = []
 
 new_lists = [n51s, n76s, n101s]
 
-names = ['alg', 'ant_count', 'its', 'seed', 'a_ant', 'n_ant', 'r', 'ch_ants',
+names = ['alg', 'ant_count', 'its', 'seed', 'a_ant', 'b_ant', 'r', 'ch_ants',
          'a_pso', 'b_pso', 'g', 'score', 'time', 'best_it']
 
 for i, file_all in enumerate(files_all):
@@ -53,3 +53,49 @@ for i, file_all in enumerate(files_all):
             dic[names[j]] = value
         new_lists[i].append(dic)
 
+# for a in n51s:
+#     print(a)
+
+its = [50, 100, 150, 200]
+algs = ['standard', 'elitist', 'rank', 'pso', 'greedy', 'random']
+seeds = [6666, 7777, 8888, 9213]
+
+
+# usredniamy: score, time, best_it
+
+def make_average_for_graph(graph):
+    data = None
+    if graph == '51':
+        data = n51s
+    elif graph == '76':
+        data = n76s
+    elif graph == '101':
+        data = n101s
+    for alg in algs:
+        for it in its:
+            filename = f"av_{alg}_{it}.txt"
+            av_score = 0
+            av_time = 0
+            av_best_it = 0
+            if alg != 'greedy' and alg != 'random':
+                rows = list(filter(lambda x: x['its'] == str(it) and x['alg'] == str(alg), data))
+            else:
+                rows = list(filter(lambda x: x['alg'] == str(alg), data))
+            if not rows:
+                break
+
+            if alg != 'greedy':
+                for i in range(len(seeds)):
+                    print(rows[i])
+                    av_score += float(rows[i]['score'])
+                    av_time += float(rows[i]['time'])
+                    av_best_it += int(rows[i]['best_it'])
+            else:
+                av_score += float(rows[0]['score'])
+                av_time += float(rows[0]['time'])
+                av_best_it += int(rows[0]['best_it'])
+            with open(filename, 'w') as file:
+                file.write(f"{av_score},{av_time},{av_best_it}")
+
+
+make_average_for_graph('51')
